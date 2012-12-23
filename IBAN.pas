b94aa851b7ed68ae5f23bcfa38d39edc9df76876
Heiko Adams <heiko@localhost.localdomain>
2012-12-23 22:02:37 +0100

    Initial check-in

---
+++ IBAN.pas
@@ -0,0 +1,341 @@
+{*******************************************************}
+{                                                       }
+{       IBAN                                            }
+{                                                       }
+{       Copyright (C) 2011 Heiko Adams                  }
+{                                                       }
+{*******************************************************}
+
+{
+     The contents of this file are subject to the Mozilla Public License
+     Version 1.1 (the "License"); you may not use this file except in
+     compliance with the License. You may obtain a copy of the License at
+     http://www.mozilla.org/MPL/
+
+     Software distributed under the License is distributed on an "AS IS"
+     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+     License for the specific language governing rights and limitations
+     under the License.
+
+     The Original Code is IBAN.pas.
+
+     The Initial Developer of the Original Code is Heiko Adams <heiko.adams@gmail.com>.
+
+     Contributor(s): ______________________________________.
+}
+
+unit IBAN;
+
+interface
+
+uses IBANMetrics;
+
+type
+	TIBAN = class
+	private
+  	FKTO: string;
+	FBLZ: string;
+  	FLand: string;
+    FIBAN: string;
+
+    FMetrics: TIBANMetrics;
+
+    procedure SetLand(const aValue: string);
+    function CodiereLandIBAN(const aLand: string): string;
+    function Modulo97PruefZiffer(const aIBAN:string):Integer;
+    function PruefeIBAN: Boolean;
+    function BerechneIBAN: string;
+    function GetLand: string;
+    function GetCountryFromIBAN: string;
+    procedure SetIBAN(const aValue: string);
+    procedure FillM97Tab;
+  public
+    property Konto: string read FKTO write FKTO;
+    property BLZ: string read FBLZ write FBLZ;
+    property Land: string read GetLand write SetLand;
+    property IBAN: string read BerechneIBAN write SetIBAN;
+    property Valid: Boolean read PruefeIBAN;
+    
+    function checkIban(const sIban: String): boolean; deprecated;
+    function IsIBAN(const s:string):boolean;
+
+	  constructor Create; override;
+    destructor Destroy; override;
+	end;
+
+var
+   m97tab:array[0..96,0..9] of byte;
+
+implementation
+
+uses SysUtils, Windows;
+
+destructor TIBAN.Destroy;
+begin
+  ZeroMemory(@FMetrics, SizeOf(FMetrics));
+  inherited;
+end;
+
+constructor TIBAN.Create;
+begin
+  inherited;
+  FillM97Tab;
+end;
+
+procedure TIBAN.SetLand(const aValue: string);
+begin
+  FLand := Trim(UpperCase(Copy(aValue, 1, 2)));
+
+  if (Length(FLand) < 2) then
+    raise Exception.CreateFmt('Invalid country code: %s', [aValue]);
+
+  ZeroMemory(@FMetrics, SizeOf(FMetrics));
+  FMetrics := GetIBANMetrics(FLand);
+end;
+
+function TIBAN.GetCountryFromIBAN: string;
+begin
+  if (Trim(FIBAN) = EmptyStr) then
+    raise Exception.Create('IBAN not set');
+
+  Result := Copy(FIBAN, 1, 2);
+end;
+
+procedure TIBAN.SetIBAN(const aValue: string);
+begin
+  if (Trim(aValue) = EmptyStr) then
+    raise Exception.Create('No IBAN submitted');
+
+	FIBAN := aValue;
+  SetLand(GetCountryFromIBAN);
+end;
+
+function TIBAN.GetLand: string;
+begin
+  if not (FLand = EmptyStr) then
+  	Result := FLand
+  else if not (FIBAN = EmptyStr) then
+  	Result := GetCountryFromIBAN
+  else
+  	raise Exception.Create('No country or IBAN set');
+end;
+
+// Original code by shima (http://www.delphipraxis.net/1061658-post6.html)
+function TIBAN.Modulo97PruefZiffer(const aIBAN:string):Integer;
+const
+   m36:string = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var
+   nCounter, nPruef : Integer;
+begin
+   Result := 0;
+
+   for nCounter := 1 to Length(aIBAN) do
+   begin
+      nPruef := Pos(aIBAN[nCounter], m36) ;
+
+      if (nPruef = 0) then
+         raise Exception.CreateFmt('Modulo97PruefZiffer(%s): invalid data', [aIBAN]);
+
+      Dec(nPruef);
+
+      if (nPruef > 9) then
+      begin
+         Result := Result * 10 + (nPruef div 10);
+         nPruef := nPruef mod 10;
+      end;
+
+      Result := Result * 10 + nPruef;
+      Result := Result mod 97;
+   end;
+end;
+
+// Code beigesteuert von Amateurprofi (http://www.delphipraxis.net/159320-iban-ueberpruefen.html#post1154665)
+procedure TIBAN.FillM97Tab;
+var 
+  i,j:Integer;
+begin
+  for i:=0 to 96 do
+    for j:=0 to 9 do
+      m97tab[i,j]:=(i*10+j) Mod 97;
+end;
+
+function TIBAN.CodiereLandIBAN(const aLand: string): string;
+var
+  sLetter: Char;
+begin
+	if (Length(Trim(aLand)) <> 2) then
+    raise Exception.CreateFmt('Invalid country code: %s', [aLand]);
+
+  for sLetter in aLand do
+    case sLetter of
+      'A': Result := Result + '10';
+      'B': Result := Result + '11';
+      'C': Result := Result + '12';
+      'D': Result := Result + '13';
+      'E': Result := Result + '14';
+      'F': Result := Result + '15';
+      'G': Result := Result + '16';
+      'H': Result := Result + '17';
+      'I': Result := Result + '18';
+      'J': Result := Result + '19';
+      'K': Result := Result + '20';
+      'L': Result := Result + '21';
+      'M': Result := Result + '22';
+      'N': Result := Result + '23';
+      'O': Result := Result + '24';
+      'P': Result := Result + '25';
+      'Q': Result := Result + '26';
+      'R': Result := Result + '27';
+      'S': Result := Result + '28';
+      'T': Result := Result + '29';
+      'U': Result := Result + '30';
+      'V': Result := Result + '31';
+      'W': Result := Result + '32';
+      'X': Result := Result + '33';
+      'Y': Result := Result + '34';
+      'Z': Result := Result + '35';
+    else
+      raise Exception.CreateFmt('Invalid country code: %s', [aLand]);
+    end;
+end;
+
+function TIBAN.PruefeIBAN(): Boolean;
+var
+	sBLZ: string;
+  sKTO: string;
+  sIBAN: string;
+  sLand: string;
+  sControl: string;
+begin
+	Result := (Length(FIBAN) = FMetrics.nLenIBAN);
+
+  if Result then
+  begin
+    sControl := Copy(FIBAN, 3, 2);
+    sBLZ := Copy(FIBAN, FMetrics.nStartBLZ, FMetrics.nLenBLZ);
+    sKTO := Copy(FIBAN, FMetrics.nStartKTO, FMetrics.nLenKTO);
+    sLand := CodiereLandIBAN(GetCountryFromIBAN);
+    sIBAN := sBLZ + sKTO + sLand + sControl;
+    Result := (Modulo97PruefZiffer(sIBAN) = 1);
+  end;
+end;
+
+function TIBAN.BerechneIBAN(): string;
+var
+  sKTO: string;
+  sIBAN: string;
+  nControl: Integer;
+  sControl: string;
+const
+	sSuffix = '00';
+  nControlBase = 98;
+begin
+  sKTO := StringOfChar('0', FMetrics.nLenKTO - Length(FKTO)) + FKTO;
+  sIBAN := FBLZ + sKTO + CodiereLandIBAN(FLand)+  sSuffix;
+  nControl := Modulo97PruefZiffer(sIBAN);
+  nControl := nControlBase - nControl;
+  
+  // 20120224 Heiko Adams
+  // make shure controlnumber has allways two characters
+  // thanks to Henry van der Mark for this hint
+  //FIBAN := FLand + IntToStr(nControl) + FBLZ + sKTO;
+  sControl := IntToStr(nControl);
+  
+  if (nControl < 10) then
+	sControl := '0' + sControl;
+	
+  FIBAN := FLand + sControl + FBLZ + sKTO;
+  
+  Result := FIBAN;
+end;
+
+// Prüfung einer IBAN auf formale Korrektheit (ohne Prüfung der Gültigkeit des Länderkürzels)
+// Autor: Dr. Michael Schramm, Bordesholm
+function checkIban(const sIban: String): boolean;
+var k,i,n,len: integer; c: char;
+    buff: array[0..67] of char;
+begin
+  result:= false;
+  n:= length(sIban);
+  
+  if (n < 5) or (n > 34) then 
+	exit;
+  
+  len:= 0; 
+  k:= 5;
+  
+  repeat // IBAN als Ziffernfolge in geänderter Reihenfolge in buff schreiben
+    c:= sIban[k];
+    
+    if (c >= '0') and (c <= '9') then 
+    begin
+      buff[len]:= c; 
+      inc(len)
+    end
+    else if (c >= 'A') and (c <= 'Z') then 
+    begin
+      i:= ord(c)-55;
+      buff[len]:= char(i div 10 + 48); 
+      inc(len);
+      buff[len]:= char(i mod 10 + 48); 
+      inc(len);
+    end
+    else 
+	  exit;
+		
+    inc(k);
+    
+    if (k > n) then 
+      k:= 1
+  until k = 5;
+  
+  i:= 0; // aktueller Rest für Modulo-Berechnung
+  
+  for k:= 0 to len-1 do 
+  begin // modulo 97 berechnen
+    i:= (i * 10 + ord(buff[k]) - 48) mod 97;
+  end;
+  
+  result:= (i = 1)
+end;
+
+// Code beigesteuert von Amateurprofi (http://www.delphipraxis.net/159320-iban-ueberpruefen.html#post1154665)
+function TIBAN.IsIBAN(const s:string):boolean;
+var 
+  len: integer;
+  cs: byte;
+  function GetCheckSum(first,last:integer):boolean;
+  var 
+    i: integer; 
+    c: integer;
+  begin
+    for i:=first to last do 
+    begin
+	  c:=Ord(s[i])-48;
+	  
+	  case c of
+		 0..9     :  cs:=m97tab[cs,c];
+		 17..42   :  cs:=m97tab[m97tab[cs,(c-7) Div 10],(c-7) Mod 10];
+		 else Exit(False);
+	  end;
+   end;
+   result:=true;
+end;
+begin
+  len:=Length(s);
+  
+  if (len<5) or (len>34) then 
+    Exit(false);
+  
+  cs:=0;
+  
+  if not GetCheckSum(5,len) then 
+    Exit(false);
+  
+  if not GetCheckSum(1,4) then 
+    Exit(false);
+  
+  result:=cs=1;
+end;
+
+end.
---
+++ IBANMetrics.pas
@@ -0,0 +1,179 @@
+{*******************************************************}
+{                                                       }
+{       IBANMetrics                                     }
+{                                                       }
+{       Copyright (C) 2011 Heiko Adams                  }
+{                                                       }
+{*******************************************************}
+
+
+{
+     The contents of this file are subject to the Mozilla Public License
+     Version 1.1 (the "License"); you may not use this file except in
+     compliance with the License. You may obtain a copy of the License at
+     http://www.mozilla.org/MPL/
+
+     Software distributed under the License is distributed on an "AS IS"
+     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+     License for the specific language governing rights and limitations
+     under the License.
+
+     The Original Code is IBANMetrics.pas.
+
+     The Initial Developer of the Original Code is Heiko Adams <heiko.adams@gmail.com>.
+
+     Contributor(s): ______________________________________.
+}
+
+unit IBANMetrics;
+
+interface
+
+type
+  TIBANMetrics = packed record
+    nLenIBAN: Word;
+    nStartBLZ: Word;
+    nLenBLZ: Word;
+    nStartKTO: Word;
+    nLenKTO: Word;
+  end;
+
+function GetIBANMetrics(const aLand: string): TIBANMetrics;
+
+implementation
+
+uses SysUtils;
+
+function GetIBANMetrics(const aLand: string): TIBANMetrics;
+begin
+  {
+  	Übersicht über die Struktur der IBAN in verschiedenen Ländern:
+  	https://secure.wikimedia.org/wikipedia/de/wiki/International_Bank_Account_Number#IBAN-Struktur_in_verschiedenen_L.C3.A4ndern
+  }
+
+	if (aLand = 'AT') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 20;
+      nLenBLZ := 5;
+      nLenKTO := 11;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else 	if (aLand = 'BE') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 16;
+      nLenBLZ := 3;
+      nLenKTO := 5;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'CH') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 21;
+      nLenBLZ := 5;
+      nLenKTO := 12;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'DE') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 22;
+      nLenBLZ := 8;
+      nLenKTO := 10;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else 	if (aLand = 'DK') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 18;
+      nLenBLZ := 4;
+      nLenKTO := 10;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else 	if (aLand = 'FR') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 27;
+      nLenBLZ := 5;
+      nLenKTO := 11;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'LI') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 21;
+      nLenBLZ := 5;
+      nLenKTO := 12;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'LU') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 20;
+      nLenBLZ := 3;
+      nLenKTO := 17;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'NL') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 18;
+      nLenBLZ := 4;
+      nLenKTO := 10;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'CZ') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 24;
+      nLenBLZ := 4;
+      nLenKTO := 16;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else if (aLand = 'PL') then
+  begin
+  	with Result do
+    begin
+      nLenIBAN := 28;
+      nLenBLZ := 7;
+      nLenKTO := 16;
+      nStartBLZ := 5;
+      nStartKTO := nStartBLZ + nLenBLZ;
+    end;
+  end
+  else
+  	raise Exception.CreateFmt('Country (%s) not supported yet', [aLand]);
+end;
+
+end.
